47830ae8b90501af35b92cb8c68995920cffc0c9
Zillion <DevZillion@proton.me>
2024-02-20 22:08:40 +0200

    gta7lite: drop device.mk usage

--- twrp_gta7litewifi.mk
+++ twrp_gta7litewifi.mk
@@ -24,8 +24,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # For PitchBlack use vendor/pb/config/common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/gta7litewifi/device.mk)
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # Charger
 PRODUCT_PACKAGES += \
